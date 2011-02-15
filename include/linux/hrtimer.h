f568c829d4eb504699f25a491008246c1bc380d3
John Stultz <john.stultz@linaro.org>
2011-02-14 17:52:09 -0800

    time: Introduce get_monotonic_boottime and ktime_get_boottime
    This adds new functions that return the monotonic time since boot
    (in other words, CLOCK_MONOTONIC + suspend time).
    
    CC: Jamie Lokier <jamie@shareable.org>
    CC: Thomas Gleixner <tglx@linutronix.de>
    CC: Alexander Shishkin <virtuoso@slind.org>
    CC: Arve Hjønnevåg <arve@android.com>
    Signed-off-by: John Stultz <john.stultz@linaro.org>

--- include/linux/hrtimer.h
+++ include/linux/hrtimer.h
@@ -316,7 +316,8 @@ static inline int hrtimer_is_hres_active(struct hrtimer *timer)
 
 extern ktime_t ktime_get(void);
 extern ktime_t ktime_get_real(void);
-
+extern ktime_t ktime_get_update_offsets(ktime_t *offs_real);
+extern ktime_t ktime_get_boottime(void);
 
 DECLARE_PER_CPU(struct tick_device, tick_cpu_device);
 
